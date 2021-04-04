01d8c6ecef45d1f24ab358434f4d114eca0c8374
Xiaochen Xu <xiaochex@gmail.com>
2021-04-04 09:58:38 -0400

    commit tree d84520cc031888bdfff6fcb812adc75015227e19

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+ function submitData(name,email){
+     return fetch("http://localhost:3000/users", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+         } ,
+         body: JSON.stringify({
+             name,
+             email
+         })
+     }) 
+    .then(res => res.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(error => document.body.innerHTML = error.message)
+ }
+
+
+
+
+
